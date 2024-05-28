82b0d73a58942d5aefe4d492422ae8f2cd9bf07b
Pritha Sen <prithasen006@gmail.com>
2024-05-28 12:01:22 +0530

    The issue is not fixed.

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -56,9 +56,6 @@ const ExpenseForm = ({ onAddExpense, buttonColor }: ExpenseFormProps,) => {
       <form onSubmit={handleSubmit(onSubmit)} style={{ padding: "0 20px" }}>
         <div style={{ marginBottom: "10px" }}>
           <DatePicker
-           {...register("date", {
-            required: "Please enter the date",
-          })}
             selected={selectedDate}
             onChange={(date: Date | null) => setSelectedDate(date)}
             dateFormat="yyyy-MM-dd"
